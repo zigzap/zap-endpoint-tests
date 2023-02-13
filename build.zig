d0dedfe08c1a19f46cd3e7a02a2662b9f69c9d8b
Rene Schallner <rene@renerocks.ai>
2023-02-13 16:40:12 +0100

    upgraded to using zig packages

--- build.zig
+++ build.zig
@@ -1,28 +1,29 @@
 const std = @import("std");
-const zap_builder = @import("./libs/zap/build.zig");
-
-const zap = std.build.Pkg{
-    .name = "zap",
-    .source = std.build.FileSource{ .path = "./libs/zap/src/zap.zig" },
-};
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("zap-endpoint-tests", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    const facil_dep = b.dependency("facil.io", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
-    exe.addPackage(zap);
-    zap_builder.addZap(exe, "./libs/zap/") catch unreachable;
+    const zap = b.dependency("zap", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "zap-endpoint-tests",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.addModule("zap", zap.module("zap"));
+    exe.linkLibrary(facil_dep.artifact("facil.io"));
     exe.install();
 
     const run_cmd = exe.run();
@@ -34,9 +35,10 @@ pub fn build(b: *std.build.Builder) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+    });
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
